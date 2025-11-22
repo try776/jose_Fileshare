f3ada17ab2c56e620dbb2cef8bff3de7559b59ab
Your Name <you@example.com>
2025-11-22 16:52:23 +0100

    set up gen 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Amplify } from 'aws-amplify'
-import amplifyconfig from './amplifyconfiguration.json' // Diese Datei wurde automatisch erstellt
+import outputs from '../amplify_outputs.json' // NEU: Gen 2 Config
 import './index.css'
 import App from './App.jsx'
 
-// Amplify konfigurieren
-Amplify.configure(amplifyconfig);
+Amplify.configure(outputs);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
