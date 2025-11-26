853b921805d0912478de6b60076e3b068f27be44
Your Name <you@example.com>
2025-11-26 23:12:59 +0100

    changes

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { storage } from './storage/resource';
+import { data } from './data/resource'; // <--- Das hier hat gefehlt!
 
 defineBackend({
   auth,
   storage,
+  data, // <--- Das hier auch!
 });
